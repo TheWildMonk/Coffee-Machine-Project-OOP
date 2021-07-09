091aa132ce42b5214b4f300151912ff262ec1fea
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-09 16:02:17 +0600

    initial commit to a readable version of the OOP code

--- main.py
+++ main.py
@@ -3,19 +3,3 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
-main_menu = Menu()
-coffee_maker = CoffeeMaker()
-money_machine = MoneyMachine()
-
-close_machine = False
-while not close_machine:
-    user_choice = input(f"What would you like? ({main_menu.get_items()}): ")
-    if user_choice == "off":
-        close_machine = True
-    elif user_choice == "report":
-        f"{coffee_maker.report()}\n{money_machine.report()}"
-    else:
-        user_choice = main_menu.find_drink(user_choice)
-        if coffee_maker.is_resource_sufficient(user_choice):
-            money_machine.make_payment(user_choice.cost)
-            coffee_maker.make_coffee(user_choice)
