12a78709e577f79b3c9167b13c79995e0e751dce
Shuvo Nasir <shuvonasir028@gmail.com>
2021-07-09 19:35:49 +0600

    finalize the Coffee Machine Project (OOP)

--- main.py
+++ main.py
@@ -3,3 +3,21 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+# Object declaration
+main_menu = Menu()
+coffee_machine = CoffeeMaker()
+process_money = MoneyMachine()
+
+close_machine = False
+while not close_machine:
+    user_choice = input(f"What would you want? ({main_menu.get_items()}): ")
+    if user_choice == "off":
+        close_machine = True
+    elif user_choice == "report":
+        f"{coffee_machine.report()}\n{process_money.report()}"
+    else:
+        coffee = main_menu.find_drink(user_choice)
+        if coffee is not None and coffee_machine.is_resource_sufficient(coffee):
+            payment = process_money.make_payment(coffee.cost)
+            if payment:
+                coffee_machine.make_coffee(coffee)
